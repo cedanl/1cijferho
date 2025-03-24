01e19877d145ffb7a03b931fe3af87de87718de6
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-03-24 22:07:27 +0100

    Added Data Explorer

--- src/frontend/Data_Explorer.py
+++ src/frontend/Data_Explorer.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
 # -----------------------------------------------------------------------------
+# Page Configuration
+# -----------------------------------------------------------------------------
+st.set_page_config(
+    page_title="Data Explorer",
+    page_icon="🚀",
+    layout="wide",  # This sets the layout to centered (not wide)
+    initial_sidebar_state="expanded"
+)
+
+# -----------------------------------------------------------------------------
 # Main Section
 # -----------------------------------------------------------------------------
 # Main header and subtitle
-st.title("🚀 1cijferho")
+st.title("🚀 Data Explorer")
 st.write("Transform complex DUO datasets into actionable insights in minutes, not months. ✨")
--- src/main.py
+++ src/main.py
@@ -12,8 +12,9 @@ import streamlit as st
 # -----------------------------------------------------------------------------
 # Pages Overview - YOU CAN ADD MORE PAGES HERE
 # -----------------------------------------------------------------------------
-home_page = st.Page("frontend/Home.py")
-upload_files_page = st.Page("frontend/Files/Upload_Files.py")
+home_page = st.Page("frontend/Home.py", icon=":material/home:")
+upload_files_page = st.Page("frontend/Files/Upload_Files.py", icon=":material/file_upload:")
+data_explorer_page = st.Page("frontend/Data_Explorer.py", icon=":material/explore:")
 
 
 # -----------------------------------------------------------------------------
@@ -27,7 +28,7 @@ st.logo(LOGO_URL)
 # Initialize Navigation
 pg = st.navigation ( {
     "Overview": [home_page],
-    "Files": [upload_files_page]
+    "Files": [data_explorer_page, upload_files_page],
 })
 
 # -----------------------------------------------------------------------------
