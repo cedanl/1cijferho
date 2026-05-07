296afa5ba64958d5a2d84b7b186736ee21afa553
Tomer Iwan <iwantomer@hotmail.com>
2026-05-07 11:51:08 +0200

    add: vakcode to presets

--- src/eencijferho/presets.py
+++ src/eencijferho/presets.py
@@ -20,7 +20,7 @@ PRESET_CONFIGS: dict[str, dict] = {
             "opt_parquet": True,
             "opt_encrypt": True,
             "opt_snake_case": True,
-            "decode_columns": ["Hoogste vooropleiding vï¿½ï¿½r het HO", "Hoogste vooropleiding", "Opleidingscode"],
+            "decode_columns": ["Hoogste vooropleiding vï¿½ï¿½r het HO", "Hoogste vooropleiding", "Opleidingscode", "Vakcode"],
             "enrich_variables": ["Geslacht", "Hoogste vooropleiding vóór het HO"],
         },
     },
